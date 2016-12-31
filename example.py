720f15f5c0b823dc306a1cd6261c47f0429a23d0
David McNett <nugget@macnugget.org>
2016-12-31 12:22:11 -0600

    Refactor to be pure class structured without the weird zombie create

---
+++ example.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import argparse
+import asyncio
+import anthemav
+import logging
+
+log = logging.getLogger(__name__)
+
+@asyncio.coroutine
+def test():
+    parser = argparse.ArgumentParser(description=test.__doc__)
+    parser.add_argument('--host', default='127.0.0.1', help='IP or FQDN of AVR')
+    parser.add_argument('--port', default='14999', help='Port of AVR')
+    parser.add_argument('--verbose', '-v', action='count')
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        level = logging.DEBUG
+    else:
+        level = logging.ERROR
+
+    logging.basicConfig(level=level)
+
+    def log_callback(message):
+        log.info('Callback invoked: %s' % message)
+
+    host = args.host
+    port = int(args.port)
+
+    log.info('Connecting to Anthem AVR at %s:%i' % (host, port))
+
+    conn = yield from anthemav.Connection.create(host=host,port=port,loop=loop,update_callback=log_callback)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    loop = asyncio.get_event_loop()
+    asyncio.async(test())
+    loop.run_forever()
