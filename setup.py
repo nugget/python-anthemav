bf825f8365e7596c29b2cad2afa9175ea8279508
David McNett <nugget@macnugget.org>
2017-01-01 22:15:18 -0600

    Bump to v1.1.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if len(long_description) < 100:
 
 setup(
     name='anthemav',
-    version='1.1.3',
+    version='1.1.4',
     author='David McNett',
     author_email='nugget@macnugget.org',
     url='https://github.com/nugget/python-anthemav',
